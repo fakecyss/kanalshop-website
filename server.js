2f05aecd92346a9400b40cba6886291e13fa8c19
fakecyss <fakecyss@gmail.com>
2025-08-28 00:44:28 +0700

    Fix: Remove duplicate code in server.js causing SyntaxError

--- server.js
+++ server.js
@@ -3,37 +3,6 @@ const express = require('express');
 const session = require('express-session');
 const MongoDBStore = require('connect-mongodb-session')(session);
 const path = require('path');
-const fs = require('fs');
-
-const indexRouter = require('./routes/index');
-const productsRouter = require('./routes/products');
-const cartRouter = require('./routes/cart');
-const adminRouter = require('./routes/admin');
-
-const app = express();
-const port = 3000;
-
-// Load product data
-const products = JSON.parse(fs.readFileSync(path.join(__dirname, 'data', 'products.json'), 'utf8'));
-app.locals.products = products;
-
-// MongoDB Session Store
-const store = new MongoDBStore({
-    uri: process.env.MONGODB_URI,
-    collection: 'sessions'
-});
-
-// Catch errors
-store.on('error', function(error) {
-    console.error('MongoDB Session Store Error:', error);
-});
-
-// Session middleware
-require('dotenv').config();
-const express = require('express');
-const session = require('express');
-const MongoDBStore = require('connect-mongodb-session')(session);
-const path = require('path');
 
 const { MongoClient } = require('mongodb');
 
@@ -124,43 +93,3 @@ async function connectToDbAndLoadProducts() {
 }
 
 connectToDbAndLoadProducts();
-
-// View engine setup
-app.set('views', path.join(__dirname, 'views'));
-app.set('view engine', 'ejs');
-
-// Static files
-app.use(express.static(path.join(__dirname, 'public')));
-app.use(express.urlencoded({ extended: true }));
-
-// Middleware to pass cart to all views
-app.use((req, res, next) => {
-    res.locals.cart = req.session.cart || [];
-    next();
-});
-
-// Routes
-app.use('/', indexRouter);
-app.use('/products', productsRouter);
-app.use('/cart', cartRouter);
-app.use('/admin', adminRouter);
-
-// 404 handler
-app.use((req, res, next) => {
-    const err = new Error('Not Found');
-    err.status = 404;
-    next(err);
-});
-
-// Error handler
-app.use((err, req, res, next) => {
-    res.status(err.status || 500);
-    res.render('error', {
-        message: err.message,
-        error: app.get('env') === 'development' ? err : {}
-    });
-});
-
-app.listen(port, () => {
-    console.log(`Server is running on http://localhost:${port}`);
-});
